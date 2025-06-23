468ab6e6af8519965a92ae7557688a7bde5b5f21
dasharubakova <dasharubakova@yandex.ru>
2025-06-23 13:06:15 +0300

    made change with auth

--- shared/constants/authOptions.ts
+++ shared/constants/authOptions.ts
@@ -1,5 +1,7 @@
 import GitHubProvider from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
+import { prisma } from "@/prisma/prisma-client";
+import { compare } from "bcrypt";
 
 export const authOptions = {
     providers: [
@@ -13,6 +15,33 @@ export const authOptions = {
                 email: { label: "Email", type: "text" },
                 password: { label: "Password", type: "password" },
             },
+            async authorize(credentials) {
+                if (!credentials) return null;
+
+                const values = {
+                    email: credentials.email,
+                };
+
+                const findUser = await prisma.user.findFirst({
+                    where: values,
+                });
+
+                if (!findUser) return null;
+
+                const isPasswordCorrect = await compare(credentials.password, findUser.password);
+
+                if (!isPasswordCorrect) return null;
+
+                if (!findUser.verified) return null;
+
+                return {
+                    id: findUser.id,
+                    email: findUser.email,
+                    name: findUser.fullname,
+                    role: findUser.role,
+                };
+            },
         }),
     ],
+    secret: process.env.NEXTAUTH_SECRET,
 };
