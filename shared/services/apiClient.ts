1cd887df3dd055b1448ac73251e9c001183ecf49
dasharubakova <dasharubakova@yandex.ru>
2025-06-27 17:21:50 +0300

    deleted categories from api

--- shared/services/apiClient.ts
+++ shared/services/apiClient.ts
@@ -1,6 +1,5 @@
 import * as products from "./products";
 import * as ingredients from "./ingredients";
-import * as categories from "./categories";
 import * as cart from "./cart";
 import * as auth from "./auth";
 import * as stories from "./stories";
@@ -8,7 +7,6 @@ import * as stories from "./stories";
 export const Api = {
     products,
     ingredients,
-    categories,
     cart,
     auth,
     stories,
--- shared/services/categories.ts
+++
@@ -1,7 +0,0 @@
-import { axiosInstance } from "./axiosInstance";
-import { ApiRoutes } from "./constants";
-import { CategoryDTO } from "./dto/category.dto";
-
-export const getAll = async (): Promise<CategoryDTO[]> => {
-    return (await axiosInstance.get<CategoryDTO[]>(ApiRoutes.CATEGORIES)).data;
-};
